1554b77328ea3b847bd66f258245e52cee45a0ab
Tarek Benzina <tarek.benzina@cognira.com>
2018-12-25 15:25:23 +0100

    first commit

---
+++ non_personalized/mean_based_recommender.py
@@ -0,0 +1,20 @@
+
+
+class ItemMeanModelProvider:
+    def __init__(self, num_top_rated):
+        self.num_top_rated=num_top_rated
+    @property
+    def num_top_rated(self):
+        return self.__num_top_rated
+    @num_top_rated.setter
+    def num_top_rated(self, num_top_rated):
+        if not isinstance(num_top_rated,int):
+            raise TypeError("non valid type, should be an integer")
+        elif num_top_rated <= 0:
+            raise ValueError("invalid value, should be a positive integer")
+        else:
+            self.__num_top_rated = num_top_rated
+if __name__ == "__main__":
+    ITMP = ItemMeanModelProvider(0)
+    print(ITMP.num_top_rated)
+    ITMP.num_top_rated=-5
